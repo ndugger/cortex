2ad53d91edb4d60fa8783bcfc95c0b1147df4002
Nick Dugger <nick.dugger1@gmail.com>
2019-05-29 01:18:13 -0500

    toggle play/pause functionality when click on toggle cover

--- examples/video_player/components/VideoPlayer.tsx
+++ examples/video_player/components/VideoPlayer.tsx
@@ -2,17 +2,53 @@ import * as Cortex from 'cortex';
 
 import Icon from './Icon';
 
+interface VideoPlayerState {
+    paused: boolean;
+    started: boolean;
+}
+
 export default class VideoPlayer extends Cortex.Component {
+
+    private state = new Cortex.Store<VideoPlayerState>({
+        paused: true,
+        started: false
+    });
+
+    private togglePausePlay(): void {
+        const video = this.shadowRoot.getElementById('video') as HTMLVideoElement;
+
+        if (this.state.paused) {
+            video.play();
+
+            if (!this.state.started) {
+                this.state.started = true;
+            }
+        }
+        else {
+            video.pause();
+        }
+
+        this.state.paused = !this.state.paused;
+    }
+
+    protected handleComponentConnect(): void {
+        this.state.connect(this);
+    }
 
     public render(): Cortex.Node[] {
         return [
-            <HTMLDivElement id='cover'/>,
-            <HTMLDivElement id='center'>
-                <Icon glyph='play_circle_outline' size={ 64 }/>
-            </HTMLDivElement>,
-            <HTMLVideoElement>
+
+            <HTMLVideoElement id='video' onclick={ () => this.togglePausePlay() }>
                 <HTMLSourceElement src='assets/trailer.ogg' type='video/ogg'/>
-            </HTMLVideoElement>
+            </HTMLVideoElement>,
+
+            (this.state.paused && !this.state.started) && (
+                <HTMLDivElement id='cover'/>
+            ),
+
+            <HTMLDivElement id='toggle' onclick={ () => this.togglePausePlay() } hidden={ !this.state.paused }>
+                <Icon glyph='play_circle_outline' size={ 80 }/>
+            </HTMLDivElement>
         ];
     }
 
@@ -24,6 +60,26 @@ export default class VideoPlayer extends Cortex.Component {
                 display: block;
                 position: relative;
                 width: 720px;
+                z-index: 0;
+            }
+
+            #toggle {
+                align-items: center;
+                bottom: 0;
+                color: white;
+                display: flex;
+                justify-content: center;
+                left: 0;
+                position: absolute;
+                right: 0;
+                top: 0;
+                transition: all .12s ease;
+                z-index: 4;
+            }
+
+            #toggle[ hidden ] {
+                opacity: 0;
+                visibility: hidden;
             }
 
             #cover {
@@ -36,24 +92,15 @@ export default class VideoPlayer extends Cortex.Component {
                 position: absolute;
                 right: 0;
                 top: 0;
-            }
-
-            #center {
-                align-items: center;
-                bottom: 0;
-                color: white;
-                display: flex;
-                justify-content: center;
-                left: 0;
-                position: absolute;
-                right: 0;
-                top: 0;
+                z-index: 3;
             }
 
             .${ HTMLVideoElement.name } {
                 display: block;
                 outline: none;
+                position: static;
                 width: 100%;
+                z-index: 0;
             }
         `;
     }
