4825e30d53202f4bfb4cb8e24e1678e0bd9b4d26
Restyled.io <commits@restyled.io>
2020-07-15 00:33:01 +0000

    Restyled by clang-format

--- pkg/Portal.js
+++ pkg/Portal.js
@@ -1,30 +1,26 @@
-import { render } from './core/render';
-import { Component } from './Component';
-import { Fragment } from './Fragment';
+import {Component} from './Component';
+import {render} from './core/render';
+import {Fragment} from './Fragment';
+
 const portals = new Map();
 export class Portal extends Component {
-    static Mirror(props) {
+  static Mirror(props) {
         return [
             render(Portal.Reflection, { target: this }, ...props?.children ?? [])
         ];
-    }
-    render() {
-        return [
-            render(HTMLSlotElement)
-        ];
-    }
-    theme() {
-        return `
+  }
+  render() { return [ render(HTMLSlotElement) ]; }
+  theme() {
+    return `
             :host {
                 display: contents;
                 position: relative;
             }
         `;
-    }
+  }
 }
-(function (Portal) {
-    class Reflection extends Fragment {
-    }
-    Portal.Reflection = Reflection;
+(function(Portal) {
+class Reflection extends Fragment {}
+Portal.Reflection = Reflection;
 })(Portal || (Portal = {}));
 //# sourceMappingURL=Portal.js.map
